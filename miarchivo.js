7013e2588bcbd6dba06129be5890b71bbbf2624d
Joel Constantino <ignacion_constantino@outlook.es>
2022-04-21 21:01:19 -0300

    primera version de mi proyecto

---
+++ miarchivo.js
@@ -0,0 +1,11 @@
+let nombre = prompt("Ingrese el nombre");
+let contraseña = prompt("Ingrese la contraseña");
+
+while(contraseña != "123456"){
+    alert("contraseña incorresta"); 
+    contraseña = prompt("Ingrese la contraseña");
+}
+
+ if(contraseña === "123456"){
+     alert("Bienvenido " + nombre + "!")
+ }
